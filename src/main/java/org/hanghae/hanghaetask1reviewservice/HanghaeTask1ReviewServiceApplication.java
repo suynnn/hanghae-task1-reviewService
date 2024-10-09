204d7d847cdc0fd1fa94c692a67a8ab3a058717e
suynnn <suy00065@gmail.com>
2024-10-09 12:02:45 +0900

    Init: 프로젝트 첫 커밋

---
+++ src/main/java/org/hanghae/hanghaetask1reviewservice/HanghaeTask1ReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package org.hanghae.hanghaetask1reviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeTask1ReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeTask1ReviewServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hanghae/hanghaetask1reviewservice/HanghaeTask1ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hanghae.hanghaetask1reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeTask1ReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

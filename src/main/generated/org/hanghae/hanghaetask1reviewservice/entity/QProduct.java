a51130545d106aa592a9bb2514b7a0682f05a8c6
suynnn <suy00065@gmail.com>
2024-10-09 23:13:24 +0900

    Feat: QueryDsl 설정

---
+++ src/main/generated/org/hanghae/hanghaetask1reviewservice/entity/QProduct.java
@@ -0,0 +1,44 @@
+package org.hanghae.hanghaetask1reviewservice.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QProduct is a Querydsl query type for Product
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QProduct extends EntityPathBase<Product> {
+
+    private static final long serialVersionUID = 372756357L;
+
+    public static final QProduct product = new QProduct("product");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final NumberPath<Long> reviewCount = createNumber("reviewCount", Long.class);
+
+    public final ListPath<Review, QReview> reviews = this.<Review, QReview>createList("reviews", Review.class, QReview.class, PathInits.DIRECT2);
+
+    public final NumberPath<Float> score = createNumber("score", Float.class);
+
+    public QProduct(String variable) {
+        super(Product.class, forVariable(variable));
+    }
+
+    public QProduct(Path<? extends Product> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QProduct(PathMetadata metadata) {
+        super(Product.class, metadata);
+    }
+
+}
+
---
+++ src/main/generated/org/hanghae/hanghaetask1reviewservice/entity/QReview.java
@@ -0,0 +1,62 @@
+package org.hanghae.hanghaetask1reviewservice.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QReview is a Querydsl query type for Review
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReview extends EntityPathBase<Review> {
+
+    private static final long serialVersionUID = 196037122L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QReview review = new QReview("review");
+
+    public final StringPath content = createString("content");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath imageUrl = createString("imageUrl");
+
+    public final QProduct product;
+
+    public final NumberPath<Integer> score = createNumber("score", Integer.class);
+
+    public final QUser user;
+
+    public QReview(String variable) {
+        this(Review.class, forVariable(variable), INITS);
+    }
+
+    public QReview(Path<? extends Review> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QReview(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QReview(PathMetadata metadata, PathInits inits) {
+        this(Review.class, metadata, inits);
+    }
+
+    public QReview(Class<? extends Review> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.product = inits.isInitialized("product") ? new QProduct(forProperty("product")) : null;
+        this.user = inits.isInitialized("user") ? new QUser(forProperty("user")) : null;
+    }
+
+}
+

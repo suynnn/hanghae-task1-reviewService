92e195c0e9e205983e11338524afc1cadfc7dc62
suynnn <suy00065@gmail.com>
2024-10-10 18:50:40 +0900

    Feat: 비관적 락으로 동시성 제어

--- src/main/java/org/hanghae/hanghaetask1reviewservice/domain/product/repository/ProductRepository.java
+++ src/main/java/org/hanghae/hanghaetask1reviewservice/domain/product/repository/ProductRepository.java
@@ -1,9 +1,19 @@
 package org.hanghae.hanghaetask1reviewservice.domain.product.repository;
 
+import jakarta.persistence.LockModeType;
 import org.hanghae.hanghaetask1reviewservice.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Product p WHERE p.id = :productId")
+    Optional<Product> findByIdWithLock(@Param("productId") Long productId);
 }
